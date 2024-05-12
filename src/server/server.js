d72833a2b3d382da8510af641689279b058f68e1
Khairunas R W <128572429+khairunas001@users.noreply.github.com>
2024-05-12 23:19:01 +0700

    Update server.js

--- src/server/server.js
+++ src/server/server.js
@@ -8,7 +8,7 @@ const InputError = require('../exceptions/InputError');
 (async () => {
     const server = Hapi.server({
         port: 3000,
-        host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
+        host: '0.0.0.0',
         routes: {
             cors: {
                 origin: ['*'],
